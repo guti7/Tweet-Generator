d6bdbbaefe543e10ac37e6f91586e98acb01811a
José Gutiérrez <guti7@users.noreply.github.com>
2017-01-25 16:39:40 -0800

    start with modules.

---
+++ Specs/better_quote.py
@@ -0,0 +1,17 @@
+import random
+
+quotes = ("It's just a flesh wound.",
+          "He's not the Messiah. He's a very naughty boy!",
+          "THIS IS AN EX-PARROT!!")
+
+
+def random_pythong_quote():
+    rand_index = random.randint(0, len(quotes) - 1)
+    return quotes[rand_index]
+
+# Module and executables pattern
+# Using a conditiona that will only run certain code if the current
+# process file is the currenct source file
+if __name__ == '__main__':
+    quote = random_pythong_quote()
+    print(quote)
