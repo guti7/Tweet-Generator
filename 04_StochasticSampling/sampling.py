f70ab10e1335fbfdc49987ef0edfeb84ede5f866
José Gutiérrez <guti7@users.noreply.github.com>
2017-02-02 00:55:15 -0800

    add weighted distribution to probabilities of sampling.

--- 04_StochasticSampling/sampling.py
+++ 04_StochasticSampling/sampling.py
@@ -1,23 +1,34 @@
 # Given a histogram representing word frecuency distributions,
 # pull words out to produce sentences that approximate English grammar.
 
-# 1st step: Build a program that generates "sentences" from the words in
+# 1st: Build a program that generates "sentences" from the words in
 # in the histogram through random selection(stochastic sampling)
 
-# Takes a histogram and returns a single random words
-# Note: Do no not take distributions into consideration yet.
+# 2nd: Introduce weighted probabilities based on histogram data
+
+# Takes a histogram and returns a single random word
+# Note: Do not take distributions into consideration yet.
 # Example: $ python sampling.py onefish.txt
 #            fish
 import random
 import sys
 
+def probability_distribution(histogram):
+    words_probability_distr = {}
+    word_count = sum(histogram.values())
+    for word, count in histogram.iteritems():
+        words_probability_distr[word] = float(count)/word_count
+    return words_probability_distr
+
+
 def get_random_word(histogram):
-    range_max = len(histogram) - 1
-    random_index = random.randint(0, range_max)
-    # better than histogram.keys() ? returns list of keys(words)
-    list_histogram = histogram.items()
-    word_tuple = list_histogram[random_index]
-    word = word_tuple[0]
+    # range_max = len(histogram) - 1
+    # random_index = random.randint(0, range_max)
+    # # better than histogram.keys() ? returns list of keys(words)
+    # list_histogram = histogram.items()
+    # word_tuple = list_histogram[random_index]
+    # word = word_tuple[0]
+    word = random.choice([k for k in histogram for _ in range(histogram[k])])
     return word
 
 
@@ -40,7 +51,7 @@ def histogram(source_text):
     words_dictionary = set(source_text)
     return {word: source_text.count(word) for word in words_dictionary}
 
-
+# main
 if __name__ == '__main__':
     # import sys?
 
@@ -53,3 +64,7 @@ if __name__ == '__main__':
     words_list = get_words_list(filename)
     histogram = histogram(words_list)
     print get_random_word(histogram)
+    print histogram
+    probabilities = probability_distribution(histogram)
+    for k, v in probabilities.iteritems():
+        print "%-4s => %5.3f" % (k, v) # '{} {}'.format(k, v)
