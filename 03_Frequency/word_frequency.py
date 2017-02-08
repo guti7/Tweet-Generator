ae69e389fccfce606575e5b723b76f9d911868d0
José Gutiérrez <guti7@users.noreply.github.com>
2017-02-08 13:15:26 -0800

    refactor and reorganize defenitions.Add clearer comments.

--- 03_Frequency/word_frequency.py
+++ 03_Frequency/word_frequency.py
@@ -1,44 +1,44 @@
-# Analyze Word Frequency in Text
-# Create a histogram
-# Given a source body of text, we perform the following:
+# Analyze Word Frequency in Text by creating a histogram.
+# Given a source body of text, can you perform the following?:
 # What is the least/most frequent word(s)?
 # How many different words are used?
 # What is the average(mean/median/mode) frequency of words in the text?
+
 import timeit
 import string
 
 start_time = timeit.default_timer()
 
-def get_words_list(filename):
+
+# Reads from file one line at a time and returns a list of lines.
+def get_lines_list(filename):
     with open(filename, 'r') as file:
-        filein_list = file.readlines()
-        words_list = parse_words(filein_list)
-        return words_list
+        filein_lines_list = file.readlines()
+        return filein_lines_list
     # file close
 
-# parse_file -> parse_lines -> parse_words
 
-# Takes a list of lines as arguments and returns a list of words.
+# Reads from a list of lines and returns a list of individual words.
+def get_words_list(lines_list):
+    words_list = parse_words(lines_list)
+    return words_list
+
+
+# Takes a list of lines as an argument and returns a list of words.
 # It cleans up a word by removing whitespace and punctuation.
 def parse_words(input_list):
-
     words_list = []
     for line in input_list:
-        # returns list with words
         clean_line = line.translate(None, string.punctuation)
         words_list.extend(clean_line.strip().split(' '))
     return words_list
 
 
-# Takes a text source as a list and returns a histogram of word frequencies.
-def histogram(source_text):
-    # words_dictionary = set(source_text)
-    # # Does count function traverse the list each time?
-    # return {word: source_text.count(word) for word in words_dictionary}
-
-    # implementation two: a lot fastaer.
+# Takes a list of words and returns a histogram
+# as a dict type of word frequencies.
+def histogram(words_list):
     dictionary = dict()
-    for word in source_text:
+    for word in words_list:
         lowercase_word = word.lower()
         if lowercase_word == '':
             continue
@@ -49,7 +49,7 @@ def histogram(source_text):
     return dictionary
 
 
-# Takes a histogram argument and returns the count of unique words.
+# Returns the count of unique words of the given histogram.
 def unique_words(histogram):
     return len(histogram)
 
@@ -59,18 +59,25 @@ def frequency(word, histogram):
     return histogram[word] if word in histogram else 0
     # histogram.get(word, 0)
 
+
+# Print the histogram in alphabetical order
 def print_histogram_alphabetized(histogram):
-    # keys = histogram.keys()
-    # keys.sort()
-    # for key in keys:
-    #     print '%-15s : %5s' % (key, histogram[key])
+    sorted_listogram = sorted(histogram.items())
+    print_histogram(sorted_listogram)
 
-    for key in sorted(histogram):
-        print '%-15s : %5s' % (key, histogram[key])
 
+# Print the histogram in descending frequency order:
 def print_descending_frequency(histogram):
-    for key, value in sorted(histogram.iteritems(), key=lambda (k, v): (v, k), reverse = True):
-        print '%-15s : %5s' % (key, value)
+    # order only by count - not by count then word
+    frequency_listogram = sorted(histogram.iteritems(), key= lambda (word, count): (count), reverse = True)
+    print_histogram(frequency_listogram)
+
+
+# Print histogram in table format.
+# Listogram is an ordered list version of the histogram
+def print_histogram(listogram):
+    for word, count in listogram:
+        print '%-15s : %5s' % (word, count)
 
 # main
 if __name__ == "__main__":
@@ -84,9 +91,13 @@ if __name__ == "__main__":
     # continue execution
     filename = sys.argv[1]
 
-    words_list = get_words_list(filename)
+    lines_list = get_lines_list(filename)
+    words_list = get_words_list(lines_list)
     histo = histogram(words_list)
+
     print 'Analysis: ', filename
+    # print 'histogram:'
+    # print histo
 
     print 'yellow: %d' % frequency('yellow', histo)
     print 'fish: %d' % frequency('fish', histo)
@@ -95,6 +106,8 @@ if __name__ == "__main__":
     elapsed = timeit.default_timer() - start_time
     print "Elapsed Time: ", elapsed
 
-    print_histogram_alphabetized(histo)
+    print 'Alphabetical:'
+    # print_histogram_alphabetized(histo)
     print '--------------'
+    print 'Descending Frequency:'
     print_descending_frequency(histo)
